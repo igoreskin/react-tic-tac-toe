8db57f5fe1c961d182767616629ec0d850746f10
Igor Eskin <eskinhd@gmail.com>
2019-03-15 16:09:50 -0400

    Game works but withour style

--- src/components/Board.js
+++ src/components/Board.js
@@ -13,13 +13,11 @@ class Board extends Component {
 
     handleClick(i) {
         const squares = [...this.state.squares];
-        let xIsNext = this.state.xIsNext;
-        if(xIsNext) {
-            squares[i] = 'X';
-        } else {
-            squares[i] = 'O';
+        if(this.calculateWinner(squares) || squares[i]) {
+            return;
         }
-        this.setState({squares: squares, xIsNext: !xIsNext})
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({squares: squares, xIsNext: !this.state.xIsNext})
     }
 
     renderSquare(i) {
@@ -28,12 +26,40 @@ class Board extends Component {
         );
     }
 
+    calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+            }
+        }
+        return null;
+    }
+
     render() {
-        const status = 'Next player: X';
+        // const gameStatus = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+        const winner = this.calculateWinner(this.state.squares);
+        let gameStatus;
+        if(winner) {
+            gameStatus = `Winner: ${winner}`;
+        } else {
+            gameStatus = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
-                <div className="status">{status}</div>
+                <div className="status">{gameStatus}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
