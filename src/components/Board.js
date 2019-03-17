e1a6dce8357cde6a0a1840f80464581b24a4fd60
Igor Eskin <eskinhd@gmail.com>
2019-03-17 15:00:18 -0400

    Some styling changes

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,37 +4,7 @@ import './Board.css';
 
 class Board extends Component {
     // constructor(props) {
-    //     super(props);
 
-    //     this.state = {
-    //         squares: Array(9).fill(null),
-    //         xIsNext: true,
-    //         isDraw: false,
-    //         count: 0,
-    //     }
-    // }
-
-    // handleClick(i) {
-    //     let count = this.state.count;
-    //     count++;
-    //     this.setState({
-    //         count: count
-    //     })
-    //     console.log(this.state.count);
-
-    //     const squares = [...this.state.squares];
-    //     if(this.calculateWinner(squares) || squares[i]) {
-    //         return;
-    //     } 
-    //     //     else if (this.state.count === 8) {
-    //     //     console.log("no winner")
-    //     //     this.setState({
-    //     //         isDraw: true
-    //     //     })
-    //     // }
-    //     squares[i] = this.state.xIsNext ? 'X' : 'O';
-    //     this.setState({squares: squares, xIsNext: !this.state.xIsNext})
-    // }
 
     renderSquare(i) {
         return (
@@ -42,39 +12,7 @@ class Board extends Component {
         );
     }
 
-    // calculateWinner(squares) {
-    //     const lines = [
-    //         [0, 1, 2],
-    //         [3, 4, 5],
-    //         [6, 7, 8],
-    //         [0, 3, 6],
-    //         [1, 4, 7],
-    //         [2, 5, 8],
-    //         [0, 4, 8],
-    //         [2, 4, 6],
-    //     ];
-    //     for (let i = 0; i < lines.length; i++) {
-    //         const [a, b, c] = lines[i];
-    //         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-    //         return squares[a];
-    //         }
-    //     }
-    //     return null;
-    // }
-
     render() {
-
-        // const winner = this.calculateWinner(this.state.squares);
-        // let gameStatus;
-        // if(this.state.isDraw) {
-        //     gameStatus = 'This is a draw!';
-        //     return;
-        // }
-        // if(winner) {
-        //     gameStatus = `Winner: ${winner}`;
-        // } else {
-        //     gameStatus = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        // }
 
         return (
             <div className="board">
